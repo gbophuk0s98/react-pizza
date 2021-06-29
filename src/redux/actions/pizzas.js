2901b946d82917ee83559056dc579281110ca124
Surta <pasha.surta@mail.ru>
2021-06-29 16:15:57 +0300

    Reworked the query string.

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const fetchPizzas = (category, sortBy) => dispatch => {
     dispatch({ type: 'FETCH_PIZZAS_REQUEST' })
-    axios.get(`http://localhost:3001/pizzas${category ? `?category=${category}&_sort=${sortBy}&_order=desc` : `?&_sort=${sortBy}&_order=desc`}`)
+    axios.get(`http://localhost:3001/pizzas?${(category ? `category=${category}` : '') + `&_sort=${sortBy}&_order=desc`}`)
         .then(res => dispatch(setPizzas(res.data)))
 }
 
