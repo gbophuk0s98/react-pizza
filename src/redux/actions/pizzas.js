3c6f8d3a504f60216104d34d11ed84d45f0febee
Surta <pasha.surta@mail.ru>
2021-07-01 10:47:07 +0300

    changed the query string

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const fetchPizzas = (category, sortBy) => dispatch => {
     dispatch({ type: 'FETCH_PIZZAS_REQUEST' })
-    axios.get(`http://localhost:3001/pizzas?${(category ? `category=${category}` : '') + `&_sort=${sortBy}&_order=asc`}`)
+    axios.get(`/pizzas?${(category ? `category=${category}` : '') + `&_sort=${sortBy}&_order=asc`}`)
         .then(res => dispatch(setPizzas(res.data)))
 }
 
